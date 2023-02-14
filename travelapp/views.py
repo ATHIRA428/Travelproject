049ca354c833bf373b0f21131eba7768ffd517f2
ATHIRA428 <122553640+ATHIRA428@users.noreply.github.com>
2023-02-14 11:33:58 +0530

    Update views.py

--- travelapp/views.py
+++ travelapp/views.py
@@ -7,11 +7,6 @@ def fun(request):
     return render(request,"index.html",{"results":obj})
 
 
-def addition(request):
-    num1=int(request.POST["num1"])
-    num2=int(request.POST["num2"])
-    num3=num1+num2
-    return render(request,"result.html",{"add":num3})
 
 
 
